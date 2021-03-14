850aaecca2a04879d9bb8c8a8adc639e8755c50a
Peter Zatka-Haas <peter@peterzh.com>
2021-03-14 15:02:11 +0000

    Initial commit

---
+++ ivo/ivo_frappe/doctype/colour_recipe/colour_recipe.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Peter Zatka-Haas and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Colour recipe', {
+	// refresh: function(frm) {
+
+	// }
+});
