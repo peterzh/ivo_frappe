ad734d51cc972166df8ac6386d0b02ea0ad47acc
Peter Zatka-Haas <peter@peterzh.com>
2021-03-16 20:55:26 +0000

    Validate recipe proportions

--- ivo/ivo_frappe/doctype/colour_recipe/colour_recipe.py
+++ ivo/ivo_frappe/doctype/colour_recipe/colour_recipe.py
@@ -3,8 +3,13 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Colourrecipe(Document):
-	pass
+	def validate(self):
+		#Check that the proportions of child recipe items add up to 100%
+		recipe = self.as_dict()['recipe']
+		proportions = [x['proportion'] for x in recipe]
+		if sum(proportions) != 100.0:
+			frappe.throw('Proportions must sum to 100%!')
