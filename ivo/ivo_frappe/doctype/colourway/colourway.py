54e8d6eb98bb8f2ba948b2e6e2873c7cb3a5c945
Peter Zatka-Haas <peter@peterzh.com>
2021-03-16 22:56:51 +0000

    Typo

--- ivo/ivo_frappe/doctype/colourway/colourway.py
+++ ivo/ivo_frappe/doctype/colourway/colourway.py
@@ -3,8 +3,14 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Colourway(Document):
-	pass
+	def validate(self):
+		#Check that the number of colours matches the num colours given in the design
+		num_colours = len(self.colours)
+		design = frappe.get_doc('Design',self.design)
+		num_colours_design = design.number_of_colours
+		if num_colours != num_colours_design:
+			frappe.throw(f"Colourway has {num_colours} however design states {num_colours_design}")
